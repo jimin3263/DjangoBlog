b1ca077be1db4aa7cbcd1e0bc5d468d4f44bbe1f
limjimin <dkdldsla0000@naver.com>
2022-03-18 17:34:18 +0900

    refactor: CBV로 리팩토링

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from blog import views
+from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 ]
