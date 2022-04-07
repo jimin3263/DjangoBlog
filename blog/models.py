68ff8383d5e021ba046733db3b2deebe031eb8d8
limjimin <dkdldsla0000@naver.com>
2022-04-08 00:15:34 +0900

    feat: hook 추가

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    hook = models.TextField(null=True)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     attached_file = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank= True)
