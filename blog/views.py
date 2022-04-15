f7739829abe2bcb4614fcdee729687dd3f5afd1e
limjimin <dkdldsla0000@naver.com>
2022-04-15 17:09:30 +0900

    fix: 글쓰기 권한 체크

--- blog/views.py
+++ blog/views.py
@@ -81,14 +81,17 @@ def show_tag_post(request, slug):
 
     return render(request, 'blog/post_list.html', context)
 
-class PostCreate(LoginRequiredMixin, CreateView):
+class PostCreate(LoginRequiredMixin, CreateView, UserPassesTestMixin):
     model = Post
     fields = ['title', 'hook', "content", "head_image", "attached_file", "category"] #입력받을 요소들
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
 
     def form_valid(self, form):
         current_user = self.request.user
 
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author= current_user
             return super(PostCreate, self).form_valid(form)
         else:
